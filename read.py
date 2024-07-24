a12c61fed3ee7b78e9a3394812d2898e95f2faec
celery1116 <celery1116@gmail.com>
2024-07-24 18:45:06 +0800

    202407241644

--- read.py
+++ read.py
@@ -4,8 +4,12 @@ with open(file,'r') as f:
 	for line in f:
 		data.append(line)
 print('檔案'+ file +'留言資料數量為',len(data),'筆')
-
+good = []
 sum_len = 0
 for d in data:
 	sum_len += len(d)
+	if 'good' in d:
+		good.append(d)
 print('留言平均長度為', sum_len/len(data))
+
+print('一共有',len(good),'筆留言提到good')
